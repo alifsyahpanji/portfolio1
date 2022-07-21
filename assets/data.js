9e4b6cd8fb1b46c254b3cc115a5419f660352905
alifsyahpanji <alifsyahpanji@gmail.com>
2022-07-21 12:54:13 +0700

    Upload File

---
+++ assets/data.js
@@ -0,0 +1,24 @@
+const jumbotron = {
+    title: "Alifsyah Studio",
+    description: "Tempat studio musik yang seru dan asik"
+};
+
+const article = [
+    {
+        id: "about",
+        title: "About",
+        image: "assets/foto1.jpg",
+        description: "Alifsyah Studio adalah Studio Musik yang berada di Kota Semarang Indonesia, bermula dari seorangMusisi yang mendirikian usaha Studio Musik, untuk membantu para musisi lokal agar bisa latihan musik setiap hari. Dengan berjalannya waktu, usaha Studio Musik ini kian berkembang dan mulai banyak peminatnya. Setiap hari banyak musisi yang selalu berlatih di Studio ini."
+    },
+    {
+        id: "service",
+        title: "Service",
+        image: "assets/foto2.jpg",
+        description: "Kami memberikan layanan musik yang spesial. dengan alat musik yang lengkap membuat anda dapat mengekspolrasi musik sesuai hobi anda. Selain itu kami juga memberikan jasa untuk recording musik, agar anda dapat merekam hasil karya anda, dengan hasil terbaik dan biaya terjangkau"
+    }
+];
+
+const aside = {
+    title: "Alifsyah Panji Negara",
+    image: "assets/foto.jpg"
+};
